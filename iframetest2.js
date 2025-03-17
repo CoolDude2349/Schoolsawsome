0e113986620392ec5749e665c40e2aca1c3ef8ce
CoolDude2349 <rer176021@gmail.com>
2025-03-17 11:07:01 -0400

    Update and rename iframetest.js to iframetest2.js

---
+++ iframetest2.js
@@ -0,0 +1,51 @@
+document.open()
+document.write(`
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Text Box to Iframe</title>
+    <script>
+        function create() {
+           
+            var url = document.getElementById("userInput").value;
+            
+           
+            if (url) {
+                
+                var iframe = document.createElement("iframe");
+                
+               
+                iframe.src = url;
+                
+                
+                iframe.style.position = "absolute";
+                iframe.style.top = "0";
+                iframe.style.left = "0";
+                iframe.style.width = "100%";
+                iframe.style.height = "100%";
+                iframe.style.border = "none";
+                
+              
+                document.body.appendChild(iframe);
+            } else {
+                alert("Enter a valid URL.");
+            }
+        }
+    </script>
+</head>
+<body>
+    <form>
+        <label for="userInput">Enter URL:</label>
+        <input type="text" id="userInput" name="userInput" placeholder="Enter here">
+        <br>
+        <button type="button" onclick="create()">Create</button>
+    </form>
+</body>
+</html>
+
+
+`)
+document.close()
